f36db082aaceac385daca6e3fd6da2cdd22a9060
Layla <layla.kawafi@hotmail.com>
2023-11-27 17:20:04 +0000

    tweaks

--- src/scenes/projects/index.tsx
+++ src/scenes/projects/index.tsx
@@ -12,14 +12,14 @@ const projects: Array<ClassType> = [
   {
     name: "Elder Helper",
     description:
-      "Elder Helper is an app with 2 user interfaces that connects the community. I worked within a team of 6 using agile methodologies to create this mobile app. We used React Native for the front-end and Express.js to build the back-end API alongside PostreSQL for the relational database. When building the API, we rigorously tested using Jest and Supertest to ensure the reliability of the code whilst also handling any errors that could occur. We used a Google Maps Api integrated with React Native Maps to display the map view.",
+      "Elder Helper is an app with 2 user interfaces that connects the community. I worked within a team of 6 using agile methodologies to create this mobile app. We used React Native for the front-end and Express.js to build the back-end API alongside PostreSQL for the relational database. When building the API, we rigorously tested using Jest and Supertest to ensure the reliability of the code whilst also handling any errors that could occur. We used a Google Maps API integrated with React Native Maps to display the map view. There is a link which shows the app in action below.",
     image: ElderHelperPhoto,
     github: "https://github.com/layla93k/ElderHelper-frontend",
     link: "https://northcoders.com/project-phase/elder-helper",
   },
   {
     name: "NC News back-end",
-    description: "I created this API using Node.js, Express and PostgreSQ and hosted it on Render. It allows the user to carry out CRUD (create, read, update and delete) operations. There was a strong focus on Test Driven Development and following the red, green, refactor cycle. The API also allows the user to paginate data, as well as filtering and sorting. It also allows the user to post and delete comments, and increment or decrement the votes on an article.",
+    description: "I created this API using Node.js, Express and PostgreSQ and hosted it on Render. It allows the user to carry out CRUD (create, read, update and delete) operations. There was a strong focus on Test Driven Development and following the red, green, refactor cycle to enable me to write efficient, reliable code. The API allows the user to paginate data, as well as filtering and sorting. It also allows the user to post and delete comments, and increment or decrement the votes on an article.",
     image: NCBEPhoto,
     github: "https://github.com/layla93k/nc-news-backend-project",
     link: "https://nc-news-back-end-project.onrender.com/api"
@@ -27,7 +27,7 @@ const projects: Array<ClassType> = [
   {
     name: "NC News front-end",
     description:
-      "This project was created using React, CSS and hosted on Netlify and links with my previously built API. I enjoyed being able to visualise the endpoints that I had created in my previous project and getting stuck in to using various different React hooks. I am keen to add context to this project to allow a user to sign in. This web-applicaton allows the user to filter articles by different topics and sortthem into ascending or descending depending on comment count, date or likes.",
+      "This project was created using React, CSS and hosted on Netlify. It links with my previously built API. I enjoyed being able to visualise the endpoints that I had created in my previous project and getting stuck in to using various different React hooks and libraries. I am keen to add context to this project to allow a user to sign in. This web-applicaton allows the user to filter articles by different topics and sort them into ascending or descending depending on comment count, date or likes.",
     image: NCFEPhoto,
     github: "https://github.com/layla93k/fe-nc-news",
     link: "https://lk-news.netlify.app/"
@@ -35,7 +35,7 @@ const projects: Array<ClassType> = [
   {
     name: "Budget App",
     description:
-      "This was the first project I created whilst working in a team during a Code First Girls, 'Intro to JavaScript' course. I worked as part of a team to produce a small web app that allows a user to input data to trigger a change on the page. For this project we used HTML, CSS and JavaScript.",
+      "This was the first project I created whilst working in a team during a Code First Girls, 'Intro to JavaScript' course. I worked as part of a team to produce a small web app that allows a user to input data which triggers a change on the page. For this project we used HTML, CSS and JavaScript.",
     image: CFGBudgetApp,
     github: "",
     link:"https://codepen.io/layla93k/pen/KKrBGxp",
